08bd737febf34c8c3578513f47b25dc4f2737e24
JanhaviAlekar <janhavialekar@gmail.com>
2023-04-12 00:12:21 +0530

    STORE CREATION

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import store from "./store";
+import * as actions from "./actionTypes.js";
+import { bugAdded } from "./actions";
+
+const unsuscribe = store.suscribe(() => {
+    console.log("store changes! ", store.getState());
+})
+
+store.dispatch(bugAdded("bug1"));
+
+// store.dispatch({
+//     type: actions.BUG_ADDED,
+//     payload: {
+//         description: "bug1"
+//     }
+// });
+
+unsuscribe();
+
+store.dispatch({
+    type: actions.BUG_REMOVED,
+    payload: {
+        id: 1
+    }
+});
+
+console.log(store.getState());
---
+++ src/reducer.js
@@ -0,0 +1,22 @@
+let lastId = 0;
+import * as actions from "./actionTypes.js";
+
+export default function reducer(state = [], action) {
+
+    switch (action.types) {
+        case actions.BUG_ADDED :
+            return [
+                ...state,
+                {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ];
+        case actions.BUG_REMOVED:
+            return state.filter(bug => bug.id != bindActionCreators.payload.id);
+        default:
+            return state;
+    }
+
+}
